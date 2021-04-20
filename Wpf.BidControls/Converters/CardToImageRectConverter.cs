8e55596809f4f4e93151c6e0c5e6c454a93283b2
Yke Smit <44127043+YkeSmit1@users.noreply.github.com>
2021-04-20 02:55:09 +0200

    Fixed some small issues

---
+++ Wpf.BidControls/Converters/CardToImageRectConverter.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wpf.BidControls.Converters
+{
+    public class CardToImageRectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bid = (Card)value;
+            var suit = bid.Suit;
+            var face = bid.Face;
+            var topy = suit switch
+            {
+                Suit.Clubs => 0,
+                Suit.Diamonds => 294,
+                Suit.Hearts => 196,
+                Suit.Spades => 98,
+                _ => throw new ArgumentException(nameof(suit)),
+            };
+            var topx = 73 * (int)face;
+            return new Int32Rect(topx, topy, 73, 97);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
